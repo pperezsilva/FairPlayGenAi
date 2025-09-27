1449537a6acbc13fb08913ec9dc7fc5e1245b0d7
ZahidRodriguez131 <arodriguez131@ucol.mx>
2025-09-26 21:55:51 -0600

    Musica del Menu

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_menu()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,3 +25,17 @@ func _on_btn_salir_pressed() -> void:
 
 func _on_btn_creditos_pressed() -> void:
 	print("Creditos seleccionado")
+
+
+# Música del nivel
+@onready var musica: AudioStreamPlayer = $AudioStreamPlayer
+
+var musicaMenu =  preload("res://sonidos/musica/sndMenu.wav")
+
+func _menu() -> void:
+	musica.stream = musicaMenu
+	musica.play()
+	musica.finished.connect(_on_music_finished)
+
+func _on_music_finished():
+	musica.play()
