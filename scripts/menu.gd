eae96bd0c9c34c068f5c3f35b3b76f734fa31b8e
ZahidRodriguez131 <arodriguez131@ucol.mx>
2025-09-27 15:30:25 -0600

    Menu SFX Click

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,4 +1,6 @@
 extends Control
+@onready var musica: AudioStreamPlayer = $sndMenu
+@onready var click: AudioStreamPlayer = $sndClick
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,29 +15,35 @@ func _process(delta: float) -> void:
 
 func _on_btn_jugar_pressed() -> void:
 	print("Comienza el juego")
+	click.play(0.4)
+	await get_tree().create_timer(0.2).timeout # Esperar a reproducir sonido antes de ejecutar acción
 
 
 func _on_btn_opciones_pressed() -> void:
 	print("Opciones seleccionado")
+	click.play(0.4)
+	await get_tree().create_timer(0.2).timeout # Esperar a reproducir sonido antes de ejecutar acción
 
 
 func _on_btn_salir_pressed() -> void:
+	click.play(0.4)
+	await get_tree().create_timer(0.2).timeout# Esperar a reproducir sonido antes de ejecutar acción
 	get_tree().quit()
 
 
 func _on_btn_creditos_pressed() -> void:
 	print("Creditos seleccionado")
+	click.play(0.4)
+	await get_tree().create_timer(0.2).timeout# Esperar a reproducir sonido antes de ejecutar acción
 
 
-# Música del nivel
-@onready var musica: AudioStreamPlayer = $AudioStreamPlayer
-
-var musicaMenu =  preload("res://sonidos/musica/sndMenu.wav")
-
+# --- Música del nivel ---
+var musicaMenu = preload("res://sonidos/musica/sndMenu.wav")
+# Función para reproducir la música
 func _menu() -> void:
 	musica.stream = musicaMenu
 	musica.play()
 	musica.finished.connect(_on_music_finished)
-
+# Función para repetir la música
 func _on_music_finished():
 	musica.play()
