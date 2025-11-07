664f90f0e31fc18270b2d355c76bc796d4d8c9c3
Pedro Antonio Pérez Silva <pperezsilva4@gmail.com>
2025-11-06 23:59:17 -0600

    Implementacion de Mouse reactivo y ordenamiento de carpetas

--- scripts/tablon_tareas.gd
+++ scripts/tablon_tareas.gd
@@ -7,9 +7,9 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	lblReloj.text = "6:00 PM"
-	lblTexto1 = ""
-	lblTexto2 = ""
-	lblTexto3 = ""
+	lblTexto1.text = "1.- Explica una ventaja ambiental de la energía solar."
+	lblTexto2.text = "2.- Describe un reto o limitación de la energía solar."
+	lblTexto3.text = "3.- Menciona una aplicación práctica de la energía solar."
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
